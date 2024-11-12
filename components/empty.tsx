a75d908a2b0a4a5058871c18e4a654c9ae5bda81
ShreyTanna29 <147817986+ShreyTanna29@users.noreply.github.com>
2024-11-12 16:00:52 +0530

    fixed empty.tsx for small devices

--- components/empty.tsx
+++ components/empty.tsx
@@ -6,11 +6,11 @@ interface EmptyProps {
 
 function Empty({ label }: EmptyProps) {
   return (
-    <div className="h-full p-20 flex flex-col items-center justify-center">
+    <div className="h-full p-18 md:p-20 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
         <Image alt="Empty" fill src="/empty.png"></Image>
       </div>
-      <p className="text-muted-foreground text-sm text-center">{label}</p>
+      <p className="text-muted-foreground text-sm  text-center">{label}</p>
     </div>
   );
 }
