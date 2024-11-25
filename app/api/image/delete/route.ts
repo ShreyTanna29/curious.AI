72859b1919dd76e40948b54b2e2ffde6b6962046
ShreyTanna29 <147817986+ShreyTanna29@users.noreply.github.com>
2024-11-25 17:01:55 +0530

    add delete image button and functionality

---
+++ app/api/image/delete/route.ts
@@ -0,0 +1,29 @@
+import prismadb from "@/packages/api/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { url } = body;
+    console.log("==========ROUTEJS==========================");
+    console.log(url);
+    console.log("=============ROUTEJS=======================");
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    await prismadb.image.delete({
+      where: {
+        url,
+        userId,
+      },
+    });
+
+    return new NextResponse("SUCCESS", { status: 200 });
+  } catch (error) {
+    console.log("ERROR :: Delete-images ::" + error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
