ca4763783224e58877036e5c3f19947103514783
ShreyTanna29 <147817986+ShreyTanna29@users.noreply.github.com>
2024-12-05 22:40:59 +0530

    add new image page

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import axios from "axios";
-import Heading from "@/components/heading";
 import Image from "next/image";
 import {
+  ArrowUp,
+  ChevronDown,
   Download,
   EllipsisVertical,
-  Image as ImageIcon,
   Share2,
   Trash2,
 } from "lucide-react";
@@ -14,11 +14,9 @@ import * as z from "zod";
 import { formSchema } from "./constants";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import Empty from "@/components/empty";
+import { useState } from "react";
 import Loader from "@/components/loader";
 import { Card, CardFooter } from "@/components/ui/card";
 import toast from "react-hot-toast";
@@ -32,16 +30,19 @@ import {
 import { shareImage, downloadImage } from "@/packages/features";
 import { deleteImage } from "@/packages/features/deleteImage";
 import LoadingSpinner from "@/components/loadingSpinner";
+import { Textarea } from "@/components/ui/textarea";
+import { TextGenerateEffect } from "@/components/text-generate-effect";
 
 type imageType = {
   url: string;
   prompt: string;
 };
-
 function ImagePage() {
   const router = useRouter();
-  const [images, setImages] = useState<imageType[]>([]);
-  const [loadingImages, setLoadingImages] = useState(true)
+  const [newImages, setNewImages] = useState<imageType[]>([]);
+  const [prevImages, setPrevImages] = useState<imageType[]>([]);
+  const [showPrevImages, setShowPrevImages] = useState(false);
+  const [loadingImages, setLoadingImages] = useState(false)
   const [deletingImages, setDeletingImages] = useState<{
     [key: string]: boolean;
   }>({});
@@ -63,7 +64,7 @@ function ImagePage() {
       const response = await axios.get("/api/image/get-user-images");
 
       if (response.data) {
-        setImages(
+        setPrevImages(
           response.data.map((img: imageType) => {
             return { url: img.url, prompt: img.prompt };
           })
@@ -76,17 +77,13 @@ function ImagePage() {
     setLoadingImages(false)
   };
 
-  useEffect(() => {
-    userImages();
-  }, []);
-
   const isLoading = form.formState.isSubmitting;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const response = await axios.post("/api/image", values);
       const output = await response.data;
 
-      setImages((prev) => [{ url: output, prompt: values.prompt }, ...prev]);
+      setNewImages((prev) => [{ url: output, prompt: values.prompt }, ...prev]);
       form.reset();
     } catch (error: any) {
       console.log(error);
@@ -111,28 +108,24 @@ function ImagePage() {
   }
 
   return (
-    <div className="select-none">
-      <Heading
-        title="Image Generation"
-        description="Turn your thoughts into images."
-        icon={ImageIcon}
-        iconColor="text-pink-500"
-        bgColor="bg-pink-500/10"
-      />
-      <div className="px-4 lg:px-8">
-        <div>
+    <div className="select-none h-full">
+      <div className=" px-4 lg:px-8 h-full w-full">
+        <div className="w-full h-[30%] flex items-center justify-center" >
+          <TextGenerateEffect className="text-3xl md:text-6xl" words="Let's Imagify Your Thoughts ✨🎩" />
+        </div>
+        <div className="flex w-full  mt-8">
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(onSubmit)}
-              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+              className="rounded-full border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-10 gap-2"
             >
               <FormField
                 name="prompt"
                 render={({ field }) => (
-                  <FormItem className="col-span-12 lg:col-span-10">
-                    <FormControl className="m-0 p-0">
-                      <Input
-                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                  <FormItem className="col-span-10 lg:col-span-9">
+                    <FormControl className="m-0 p-2">
+                      <Textarea
+                        className="border-0 outline-none focus-visible:ring-0  focus-visible:ring-transparent"
                         disabled={isLoading}
                         placeholder="e.g. A cute cat with hat"
                         {...field}
@@ -142,10 +135,10 @@ function ImagePage() {
                 )}
               />
               <Button
-                className="col-span-12 lg:col-span-2 w-full"
+                className="rounded-full bg-black/10 dark:bg-white/10"
                 disabled={isLoading}
               >
-                Generate
+                <ArrowUp className="text-black dark:text-white" />
               </Button>
             </form>
           </Form>
@@ -156,12 +149,79 @@ function ImagePage() {
               <Loader />
             </div>
           )}
-          {!images[0] && !isLoading && !loadingImages && (
-            <Empty label="No images generated."></Empty>
-          )}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8 ">
-            {images &&
-              images.map((image) => (
+
+          <div className="flex w-full items-center justify-center">
+            <div className="cursor-pointer flex bg-black/10 p-4 items-center justify-center gap-2 dark:bg-white/10" onClick={() => {
+              setShowPrevImages(!showPrevImages)
+              userImages()
+            }}>
+              My Previous Images <ChevronDown />
+            </div>
+          </div>
+
+          <div className={` grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ${showPrevImages ? "grid" : "hidden"} `}>
+            {prevImages &&
+              prevImages.map((image) => (
+                <Card key={image.url} className="rounded-lg overflow-hidden">
+                  <div className="relative aspect-square">
+                    <div className="absolute text-white top-2 right-2 z-10 cursor-pointer ">
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <div className="p-1 rounded-full bg-black/20    backdrop-blur-sm hover:bg-black/30  transition">
+                            {(downloadingImages[image.url] || deletingImages[image.url]) ? (
+                              <LoadingSpinner />
+                            ) : (
+                              <EllipsisVertical className="w-5 h-5 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" />
+                            )}
+                          </div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                          <DropdownMenuItem
+                            className="flex gap-2 cursor-pointer"
+                            onClick={() => {
+                              imageDownloadHandler(image.url, image.prompt);
+                            }}
+                          >
+                            <Download />
+                            Download
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+
+                          <DropdownMenuItem
+                            className="flex gap-2 cursor-pointer "
+                            onClick={() => shareImage(image.url, image.prompt)}
+                          >
+                            <Share2 />
+                            Share
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem
+                            className="flex gap-2 cursor-pointer "
+                            onClick={() =>
+                              deleteImageHandler(image.url)}
+                          >
+                            <Trash2 />
+                            Delete
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </div>
+                    <Image
+                      width={524}
+                      height={524}
+                      alt="image"
+                      src={image.url}
+                    />
+                  </div>
+                  <CardFooter className="justify-center p-4 bg-black/10 dark:bg-white/10  ">
+                    <h1 className="font-bold ">{image.prompt}</h1>
+                  </CardFooter>
+                </Card>
+              ))}
+          </div>
+          <div className={` grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ${newImages ? "grid" : "hidden"} `}>
+            {newImages &&
+              newImages.map((image) => (
                 <Card key={image.url} className="rounded-lg overflow-hidden">
                   <div className="relative aspect-square">
                     <div className="absolute text-white top-2 right-2 z-10 cursor-pointer ">
