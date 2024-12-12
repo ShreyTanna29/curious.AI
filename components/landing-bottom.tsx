53f8dc03a5b33370c61da62e18f99a5682797bf3
ShreyTanna29 <shreytanna2957@gmail.com>
2024-12-12 21:38:59 +0530

    add new landing page

--- components/landing-bottom.tsx
+++ components/landing-bottom.tsx
@@ -1,6 +1,6 @@
 export default function LandingFooter() {
     return (
-        <footer className="w-full h-6 bg-white text-center">
+        <footer className="w-full h-6 text-white bg-white/10 text-center">
             All rights reserved &copy; curiuos.AI
         </footer>
     )
