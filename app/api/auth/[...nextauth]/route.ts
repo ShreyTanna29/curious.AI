54c68175a2917d286bb16196b6057fa0a71e7af2
ShreyTanna29 <shreytanna2957@gmail.com>
2024-12-16 12:57:30 +0530

    add credentials provider in next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,33 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        email: {
+          label: "Email",
+          placeholder: "Enter your email",
+          type: "email",
+        },
+        password: {
+          label: "Password",
+          placeholder: "Enter your password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        return null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export const GET = handler;
