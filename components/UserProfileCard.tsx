6f962c7caa64580a7250228b15a7f4dac8ea37df
ShreyTanna29 <shreytanna2957@gmail.com>
2024-12-24 23:16:21 +0530

    fix user avatar in sidebar on mobile view

--- components/UserProfileCard.tsx
+++ components/UserProfileCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from "./ui/card";
-import { Avatar } from "./ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { useSession } from "next-auth/react";
 
 export default function UserProfileCard() {
@@ -12,7 +12,10 @@ export default function UserProfileCard() {
         <Card className="bg-black/10 dark:bg-white/10 border-0  dark:text-white overflow-clip flex items-center">
           <CardContent className="py-6 text-ellipsis ">
             <div className="flex">
-              <Avatar />
+              <Avatar >
+                <AvatarImage src={session.data?.user?.image!} alt="User" />
+                <AvatarFallback>{userName?.charAt(0)}</AvatarFallback>
+              </Avatar>
               <div className="ml-3">
                 <p>{userName}</p>
                 <p className="text-ellipsis text-sm ">{userEmail}</p>
