99b174a02e30b0fb8c067a607db2cd34794ee9bb
ShreyTanna29 <shreytanna2957@gmail.com>
2024-12-25 21:57:25 +0530

    gsap init

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -4,8 +4,9 @@ import { BackgroundBeams } from "./background-beams";
 import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoadingSpinner from "./loadingSpinner";
+import gsap from "gsap";
 
 export default function LandingHero() {
   const [loading, setLoading] = useState(false)
@@ -13,10 +14,19 @@ export default function LandingHero() {
   const isSignedIn = status === "authenticated" ? true : false
   const router = useRouter()
 
+  useEffect(() => {
+    gsap.to("h1", {
+      opacity: 1,
+      duration: 1,
+      translateX: 0,
+    })
+  }, [])
+
+
   return (
     <div className="h-full w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased ">
       <div className="max-w-full  p-4">
-        <h1 className="relative z-10 text-2xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+        <h1 className="relative z-10 translate-x-10 opacity-0 text-2xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           Unleash the power of AI
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
@@ -39,7 +49,7 @@ export default function LandingHero() {
         {isSignedIn ? "Visit Dashboard" : "Get Started"} {loading ? <LoadingSpinner className="border-white" /> : <ArrowRight />}
       </Button>
 
-      <BackgroundBeams />
+      {/* <BackgroundBeams /> */}
 
 
     </div>
