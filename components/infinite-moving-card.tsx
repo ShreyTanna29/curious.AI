9e792aa3c053ce8d8ab774a6e97c61d442358fd3
ShreyTanna29 <shreytanna2957@gmail.com>
2025-01-01 12:56:20 +0530

    stop hover card pause

--- components/infinite-moving-card.tsx
+++ components/infinite-moving-card.tsx
@@ -8,7 +8,7 @@ export const InfiniteMovingCards = ({
     items,
     direction = "left",
     speed = "fast",
-    pauseOnHover = true,
+    pauseOnHover = false,
     className,
 }: {
     items: string[];
