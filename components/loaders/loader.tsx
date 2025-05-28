bcfd529cd8211a32b188b5aebfd5f7dfa84835a0
Prahalya <Prahek@gmail.com>
2025-05-28 17:25:41 +0530

    Issue fixed

--- components/loaders/loader.tsx
+++ components/loaders/loader.tsx
@@ -1,18 +1,10 @@
 import Image from "next/image";
-
 function Loader({ className }: { className?: string }) {
   return (
-    <div className={`relative w-16 h-16 ${className}`}>
-      <div className="w-10 h-10 absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 bg-black rounded-full">
-        <Image alt="Logo" fill src="/logo.png" className="rounded-full" />
-      </div>
+    <div className={`w-10 h-10 relative animate-spin ${className} `}>
+      <Image alt="Logo" fill src="/logo.png" />
+    </div>
 
-     <div className={`flex items-center justify-center gap-1 ${className}`}>
-      <span className="w-2 h-2 bg-black dark:bg-white rounded-full animate-bounce [animation-delay:0s]" />
-      <span className="w-2 h-2 bg-black dark:bg-white rounded-full animate-bounce [animation-delay:0.2s]" />
-      <span className="w-2 h-2 bg-black dark:bg-white rounded-full animate-bounce [animation-delay:0.4s]" />
-    </div>
-    </div>
   );
 }
 
